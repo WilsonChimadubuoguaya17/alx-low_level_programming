1e79fad1bc106724a75587bc362339e11cdb1dd5
Wilson Chimadubuoguaya Omebu <wilsonomebu@gmail.com>
2023-02-28 06:51:49 -0800

    3 Print Numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbrs - print 0123456789
+* Kindly check the code for this below
+*Return: void
+*/
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
