1c8f517d39fb8c5f6af7914fb9cf4ee77426cad0
Wilson Chimadubuoguaya Omebu <wilsonomebu@gmail.com>
2023-02-28 20:11:01 -0800

    1 Swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
 * swap_int - swaps the value of int a and int b
-* Computed by Wilson
-* the a: first int
-* the b: second int
+* Written by myself
+* @a: first int
+* @b: second int
 * Return 0
 */
 void swap_int(int *a, int *b)
